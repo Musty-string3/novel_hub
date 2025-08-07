7ea1db9b1d23729be62e679fed7356c0435987fb
Mieda0310 <miedas@colabmix.co.jp>
2025-08-07 12:53:28 +0000

    [new] settingsの認証関連の設定

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,15 +1,20 @@
+import os
+import environ
+
 from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(Path.joinpath(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!(j+^r80z9y*462ey+e#$_+f3$f6p23pt*$%&&8pqe8_0j+*-&'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -50,7 +55,10 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # templateの場所を指定する
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -101,6 +109,13 @@ CORS_ALLOWED_ORIGINS = [
 
 AUTH_USER_MODEL = "accounts.User"
 
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+## Userモデルにusernameは存在しない
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_EMAIL_REQUIRED = True
+## usernameを要求しない
+ACCOUNT_USERNAME_REQUIRED = False
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -114,7 +129,7 @@ SIMPLE_JWT = {
     # リフレッシュトークン(3日)
     'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     # 認証タイプ
-    'AUTH_HEADER_TYPES': ('JWT', ),
+    'AUTH_HEADER_TYPES': ('Bearer', ),
     # 認証トークン
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken', )
 }
@@ -135,9 +150,41 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+##### STATIC #####
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    [
+        os.path.join(BASE_DIR, "static"), 
+    ]
+)
+
+##### MEDIA #####
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+##### Gmail設定 #####
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_OPERATION = os.getenv('EMAIL_OPERATION')
+
+
+# ログイン/ログアウト後の遷移先を設定
+# LOGIN_URL = '/login/'
+# LOGIN_REDIRECT_URL = '/'
+
+# LOGOUT_URL = '/logout/'
+# ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
+
+BASE_URL = env("BASE_URL")
+FRONTEND_URL = env("FRONTEND_URL")
