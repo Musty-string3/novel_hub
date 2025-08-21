223e0d98b1dd9a46972f18c6187af17c20e8b367
Musty-string3 <shuya199309@gmail.com>
2025-08-21 12:35:43 +0000

    [update] フロント側でユーザー情報の取得に失敗したらログイン画面に遷移させるように修正

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -15,11 +15,28 @@ export const authApi = () => {
     // TODO: ローカルストレージに保存していたアクセストークンを削除（そもそもローカルストレージには保存したくないので修正する）
     const accessToken = localStorage.getItem("accessToken");
 
-    return axios.create({
+    const request = axios.create({
         baseURL,
         headers: {
             "Content-Type": "application/json",
             Authorization: accessToken ? `Bearer ${accessToken}` : "",
         }
-    })
+    });
+
+    // サーバーからログインしているかの認証結果を返却された際の動作
+    request.interceptors.response.use(
+        (response) => response,
+        (error) => {
+            // ログインユーザーの認証に失敗したらログイン画面に遷移させる
+            if (error?.response?.status === 401) {
+                // ログイン画面の時は動作させない
+                if (window.location.pathname !== "/login") {
+                    window.location.href = "/login";
+                }
+            }
+            return Promise.reject(error);
+        }
+    );
+
+    return request;
 };
